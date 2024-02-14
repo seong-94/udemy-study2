69719479fa465a1173da7a44d3e3bfd464624c5c
seong-94 <togussla@naver.com>
2024-02-14 23:53:19 +0900

    테일윈드 설정 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
 
 export default function Header() {
   return (
-    <header>
+    <header className="flex flex-col items-center mt-8 mb-16">
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
       <p>A community of artists and art-lovers.</p>
